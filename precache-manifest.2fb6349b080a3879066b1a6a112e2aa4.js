fefc8ed1e1c0f1afb8f0f9139c6228816b296df6
m-mizen <mark.c.mizen@gmail.com>
2020-09-26 22:26:27 +0000

    Updates

---
+++ precache-manifest.2fb6349b080a3879066b1a6a112e2aa4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64d7c598c8cb37671aea58cc1994b93b",
+    "url": "/react-book-search/index.html"
+  },
+  {
+    "revision": "2d580365acbd1517748f",
+    "url": "/react-book-search/static/css/main.ee89f203.chunk.css"
+  },
+  {
+    "revision": "bcfccaacfd38f5fb81d1",
+    "url": "/react-book-search/static/js/2.6ee9aff0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-book-search/static/js/2.6ee9aff0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d580365acbd1517748f",
+    "url": "/react-book-search/static/js/main.a519d3d0.chunk.js"
+  },
+  {
+    "revision": "90beb743e06c4bdba228",
+    "url": "/react-book-search/static/js/runtime-main.1c1002c1.js"
+  }
+]);
